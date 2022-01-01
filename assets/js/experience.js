a9eed5c60d8329d28bd7fbd1dd56475d41d84308
Ankit <jhaankitkumar12@gmail.com>
2022-01-01 10:34:07 +0530

    files added

---
+++ assets/js/experience.js
@@ -0,0 +1,243 @@
+AOS.init();
+
+//  Work experience cards
+
+
+const experiencecards = document.querySelector(".experience-cards");
+const exp = [
+  {
+    title: "User Experience Design Intern",
+    cardImage: "assets/images/experience-page/WebbRes.png",
+    place: "WebbRes",
+    time: "(Aug, 2021 - present)",
+    desp: "<li>Mainly responsible for designing Version 2 for the company's software</li> <li>Created design system for this B2B SaaS software. </li> <li>Conducting User Research to design Wireframes, creating Mockups & building prototypes.</li>",
+  },
+  {
+    title: "UI/UX Designer & Co-founder",
+    cardImage: "assets/images/experience-page/Linkonnekt.png",
+    place: "Linkonnekt",
+    time: "(Jun - Aug, 2021)",
+    desp: "<li>It was my first startup with BITS Goa friends. </li><li>Designed the first iteration of Linkonnekt which was for web version. </li><li>Performing research work which includes user survey & competitive analysis. </li>",
+  },
+  {
+    title: "User Expeience Designer (Freelance)",
+    cardImage: "assets/images/experience-page/Reyak.png",
+    place: "Reyak - a social media app",
+    time: "(May - Jun, 2021)",
+    desp:"<li>It was my first freelance project regarding implementation of UI & UX. </li><li>I was solely responsible for performing user reseach and make the application as per user's need.</li> <li>Designed easy and minimal UI to engage people and prototyped it make it look functional.</li> ",
+  },
+];
+
+const showCards2 = () => {
+  let output = "";
+  exp.forEach(
+    ({ title, cardImage, place, time, desp }) =>
+      (output += `        
+    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+      <div class="card card1">
+        <img src="${cardImage}" class="featured-image"/>
+        <article class="card-body">
+          <header>
+            <div class="title">
+              <h3>${title}</h3>
+            </div>
+            <p class="meta">
+              <span class="pre-heading">${place}</span><br>
+              <span class="author">${time}</span>
+            </p>
+            <ol>
+              ${desp}
+            </ol>
+          </header>
+        </article>
+      </div>
+    </div>
+      `)
+  );
+  experiencecards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards2);
+
+
+// Volunteership Cards
+
+const volunteership = document.querySelector(".volunteership");
+const volunteershipcards = [
+  {
+    title: "Pankh - Wings that Connect",
+    cardImage: "assets/images/experience-page/pankh.png",
+    description:
+      "Pankh helps financially weak students by connecting them with Resources.",
+  },
+  {
+    title: "Webpage for Real Estate Website",
+    cardImage: "assets/images/experience-page/Homey.png",
+    description:
+      "Responsible for designing a creative and unique home page for a Real-Estate Website.",
+  },
+  {
+    title: "Soccer Stats Website",
+    cardImage: "assets/images/experience-page/SocStat.png",
+    description:
+      "Designed & Developed a fully responsive website for showing soccer stats. ",
+  },
+  {
+    title: "Music Website",
+    cardImage: "assets/images/experience-page/SoulSound.png",
+    description:
+      "Designed & developed music website like spotify and integrated it with voice API within search bar.",
+  },
+  {
+    title: "EnLock- Password Protection App",
+    cardImage: "assets/images/experience-page/enlock.png",
+    description:
+      "Application which helps others to secure their information, data and any other thing which they consider to remain private.",
+  },
+  {
+    title: "Inspect It - Inspection App",
+    cardImage: "assets/images/experience-page/inspect.png",
+    description:
+      "Designed a quiz app which is used for inspection purpose.",
+  },
+];
+
+const showCards = () => {
+  let output = "";
+  volunteershipcards.forEach(
+    ({ title, cardImage, description }) =>
+      (output += `        
+      <div class="card volunteerCard" data-aos="fade-down" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="600" style="height: 550px;width:400px">
+      
+      <img src="${cardImage}" height="250" width="65" class="card-img" style="border-radius:10px">
+      <div class="content">
+          <h2 class="volunteerTitle">${title}</h2><br>
+          <p class="copy">${description}</p></div>
+      
+      </div>
+      `)
+  );
+  volunteership.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+
+// Hackathon Section
+
+
+const hackathonsection = document.querySelector(".hackathon-section");
+const mentor = [
+  {
+    title: "The Uplift Project",
+    subtitle: "Mentor",
+    image: "assets/images/experience-page/uplift.png",
+    desp: "I would be responsible for guiding 100s of participants across the globe in their first steps towards Applied Data Science and building end-to-end machine learning projects.",
+  },
+  {
+    title: "ULHacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/ulhacks.png",
+    desp: "ULHacks is a 36 hour long hackathon organized with an aim to enable students to create a project which empowers learning.",
+  },
+  {
+    title: "WaffleHacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/wafflehacks.png",
+    desp: "WaffleHacks 2021 is an upcoming 48-hour student-organized hackathon with an aim to bring technical solutions to local communities and small businesses.",
+  },
+  {
+    title: "Elevate Tech",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/elevate.png",
+    desp: "Elevate Hacks is an upcoming all-female virtual hackathon coming from AUGUST 14th to 15th.",
+  },
+  {
+    title: "PitchTeen",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/pitchteen.png",
+    desp: "PitchTeen 2.0 was a tech startup competition for female and non-binary communities in high school and college from all over the world with an aim to create a safe and encouraging place for women to help them explore tech and business fields.",
+  },
+  {
+    title: "Hack-A-Solution",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/hackasolution.png",
+    desp: "Hack-a-Solution 2021 is a 24 hour long hackathon organized by Frisco students to raise awareness of world issues through technology.",
+  },
+  {
+    title: "UniGlobe Hacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/uniglobe.png",
+    desp: "UniGlobe Hacks 2.0 is an upcoming 4-day long hackathon run by high school students held during 28-31 July 2021.",
+  },
+  {
+    title: "AtlasHacks",
+    subtitle: "Mentor",
+    image: "assets/images/experience-page/atlas.png",
+    desp: "AtlasHacks II is a 3-day virtual hackathon that aims to foster creativity within hackers all around the world.",
+  },
+  {
+    title: "NeoHacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/neo.png",
+    desp: "NeoHacks is an upcoming 48-hour hackathon taking place July 23-25, 2021 where hundreds of students from around the world will come together virtually to learn new skills, meet new friends, and build awesome projects. We provide beginner-friendly workshops, mentorship, fun games, and more.",
+  },
+  {
+    title: "Mission Inspired",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/mission.png",
+    desp: "InspireEd Hacks is a nationwide initiative organized by the student-for-student nonprofit, Mission InspirEd, challenging students ages 10-18 to synthesize their creativity and coding experience into a product beneficial to the educational community.",
+  },
+  {
+    title: "Hack3",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/hack3.png",
+    desp: "Hack3 2021 is an upcoming global online hackathon that aims to enable high school students to learn computer science, with a broader message of (everyone can code) targeting underrepresented demographics to improve diversity and inclusion of individuals in the computer technology space.",
+  },
+  {
+    title: "JITHack",
+    subtitle: "Mentor",
+    image: "assets/images/experience-page/jithack.png",
+    desp: "JITHack is an upcoming virtual design and development hackathon organized by the students of Jyothy Institute of Technology, Bengaluru on June 26, 2021, where participants have 24 hours to build something incredible.",
+  },
+  {
+    title: "Recess Hacks",
+    subtitle: "Mentor",
+    image: "assets/images/experience-page/recess.png",
+    desp: "Recess Hacks is an upcoming hackathon for high school students in the Greater Toronto Area that strives to encourage innovation among high school students and get involved in technology.",
+  },
+  {
+    title: "Citro Tech",
+    subtitle: "Mentor",
+    image: "assets/images/experience-page/citro.png",
+    desp: "Citro Hacks 2021 is an upcoming all-inclusive online hackathon organized by Citro Tech, a student led organization dedicated to empowering minorities in the tech field</li>",
+  },
+  {
+    title: "NHacks",
+    subtitle: "Judge",
+    image: "assets/images/experience-page/nhacks.png",
+    desp: "NHacks VI was a hackathon that aimed to encourage students everywhere to develop skills in computer science by creating tangible products that they're proud of. It initially started as Michigan's largest high school hackathon.",
+  }
+  
+
+];
+
+const showCards3 = () => {
+  let output = "";
+  mentor.forEach(
+    ({ title, image, subtitle, desp}) =>
+      (output += `  
+      <div class="blog-slider__item swiper-slide">
+        <div class="blog-slider__img">
+            <img src="${image}" alt="">
+        </div>
+        <div class="blog-slider__content">
+          <div class="blog-slider__title">${title}</div>
+          <span class="blog-slider__code">${subtitle}</span>
+          <div class="blog-slider__text">${desp}</div>
+          <a href="#" class="blog-slider__button">Read More</a>   
+        </div>
+      </div>
+      `)
+  );
+  hackathonsection.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards3);
